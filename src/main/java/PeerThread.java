2a28f0aefef0c38c6878c624ff391d0c63ab6628
sandraerian16 <36929017+sandraerian16@users.noreply.github.com>
2018-05-10 01:59:19 +0200

    Update PeerThread.java

--- src/main/java/PeerThread.java
+++ src/main/java/PeerThread.java
@@ -1,39 +1,64 @@
-import org.apache.commons.lang3.SerializationUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.List;
 
 
 public class PeerThread extends Thread{
 
-	private Blockchain blockchain;
-	
+	private MyLocalPeer myLocalPeer;
 	protected Socket socket;
-	private InputStream inputStream;
-	private OutputStream outputStream;
 
-	
-	
-	public PeerThread(Socket socket, Blockchain blockchain) throws IOException {
-		this.blockchain = blockchain;
+	private ObjectInputStream objectInputStream;
+	private ObjectOutputStream objectOutputStream;
+
+	private int requestNum;
+	private int longestValidBlockChain = myLocalPeer.getBlockchain().getBlockChainSize();
+
+	public PeerThread(Socket socket, MyLocalPeer myLocalPeer) throws IOException {
+		this.myLocalPeer = myLocalPeer;
 		this.socket = socket;
-
 	}
 
 	public void run(){
+		try {
+			objectInputStream  = new ObjectInputStream(socket.getInputStream());
+			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 
-			try {
-				byte[] buffer = null;
-				inputStream = this.socket.getInputStream();
-				outputStream = this.socket.getOutputStream();
-				inputStream.read(buffer);
-				Block block = SerializationUtils.deserialize(buffer);
-				blockchain.getBlocks().add(block);
-				System.out.println("bravo 3lik "+ block.getHash());
-			} catch (Exception e) {
-				e.printStackTrace();
+			requestNum = objectInputStream.readInt();
+
+			if(requestNum == 1){ // receive block
+				myLocalPeer.addBlock((Block) objectInputStream.readObject());
+			}
+			else if(requestNum == 2){ // want longest valid block chain size
+				objectOutputStream.writeInt(3);
+				objectOutputStream.writeInt(myLocalPeer.getBlockchain().getBlockChainSize());
+			}
+			else if(requestNum == 3){ // send longest valid block chain size
+				int validBlockChainSizeTemp = objectInputStream.readInt();
+
+				if(validBlockChainSizeTemp > longestValidBlockChain)
+					longestValidBlockChain = validBlockChainSizeTemp;
+
+				myLocalPeer.setLongestValidBlockChain(longestValidBlockChain);
+			}
+			else if(requestNum == 4){ // send block chain request
+				objectOutputStream.writeInt(6);
+				myLocalPeer.sendBlockChain(objectOutputStream);
+			}
+			else if(requestNum == 5){ // send IP Addresses
+				myLocalPeer.setNetworkPeers((List<String>) objectInputStream.readObject());
+			}
+			else if(requestNum == 6){ // i receive a BlockChain
+				myLocalPeer.setBlockchain((Blockchain) objectInputStream.readObject());
+			}
+			else if(requestNum == 7){ //i receive transactions
+				myLocalPeer.appendTransactions((List<Transaction>) objectInputStream.readObject());
+			}
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
 
 		}
 	}
