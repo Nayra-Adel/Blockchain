21eee179de4240793efdee9b92f8ecd1d56716b4
sandraerian16 <36929017+sandraerian16@users.noreply.github.com>
2018-05-10 01:59:53 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,54 @@
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    private List<String> networkPeers = new ArrayList<>();
+    private List<String> networkPeers = new ArrayList<String>();
 
     public static void main(String[] args) throws Exception {
 
 
-        Blockchain blockchain = new Blockchain();
+        MyLocalPeer myLocalPeer = new MyLocalPeer();
 
-        Thread thread = new MulticastReceiver();
-        thread.start();
+        Thread generateTransactions = new GenerateTransactions(myLocalPeer.getBlockchain().getPendingTransaction());
+        generateTransactions.start();
 
-       // Thread my_peer= new MyLocalPeer();
-       // my_peer.start();
+        Thread multicast = new Multicast(myLocalPeer);
+        multicast.start();
+
+        Thread my_peer= new MyLocalPeer();
+        my_peer.start();
 
 
-
-       /* blockchain.createTransaction(new Transaction("Nayra","Reham",500));
-        blockchain.createTransaction(new Transaction("Sandra","Nayra",1000));
-
-        System.out.println("balance "+ blockchain.checkBalance("Reham"));
-
-        blockchain.minePendingTransaction("Reham");
-
-        System.out.println("Rehame's balance (before) : "+ blockchain.checkBalance("Reham"));
-
-        blockchain.minePendingTransaction("Reham");
-        System.out.println("Rehame's balance (after) : "+ blockchain.checkBalance("Reham"));
-        System.out.println("Nayra's balance "+ blockchain.checkBalance("Nayra"));
-        System.out.println("Sandra's balance "+ blockchain.checkBalance("Sandra"));*/
-
+        Scanner input = new Scanner(System.in);
+        label:
+        while (true) {
+            System.out.println("please choose from the following menu :) enter the number  ");
+            System.out.println("1-Query the longest valid blockchain on the network.");
+            System.out.println("2-Request another Peer’s copy of the blockchain.");
+            System.out.println("3-Broadcast transactions to miners to add it on new Blocks.");
+            System.out.println("4-Mine block.");
+            System.out.println("5-exit");
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    myLocalPeer.sendLongestValidBCSizeRequest();
+                    System.out.println(myLocalPeer.getLongestValidBlockChain());
+                    break;
+                case 2:
+                    myLocalPeer.sendBlockChainRequest("");
+                    break;
+                case 3:
+                    myLocalPeer.sendTransactions();
+                    break;
+                case 4:
+                    myLocalPeer.getBlockchain().minePendingTransaction(myLocalPeer.getName());
+                    myLocalPeer.sendBlock();
+                    break;
+                default:
+                    break label;
+            }
+        }
     }
 }
